8a829496afa6b48829f61b951da32c3c0d672da8
michel-flexcode <michelseocode@gmail.com>
2023-11-15 15:52:52 +0100

    V5 Blog

--- main.js
+++ main.js
@@ -1,32 +1,33 @@
 
-import './style.css'
-import { data } from './public/data.json';
+import './style.css';
+import data from './public/data.json';
 import { nav } from './nav';
 
-const listePersonnes = () => {
+const ListeArticles = () => {
   let html = '';
   for (let i = 0; i < data.length; i++) {
-    const personne = data[i];
-    let personneCard = `
-      <a class="card col-5 col-md-3" href="/personne/?id=${personne.id}">
-        <img src="${personne.avatar}" class="card-img-top" alt="avatar de ${personne.prenom} ${personne.nom}">
-        <div class="card-body">
-          <h5 class="card-title">${personne.prenom} ${personne.nom}</h5>
+    const article = data[i];
+
+    // <img src="${article.avatar}" class="card-img-top" alt="avatar de ${article.prenom} ${article.nom}"></img>
+    let articleCard = `
+      <a href="/article/?id=${article.id}">
+        <div>
+          <h5>${article.author}</h5>
+          <p>${article.content}</p>
         </div>
       </a>
     `;
-    html += personneCard;
+    html += articleCard;
   }
   return html;
 };
 
 document.querySelector('#app').innerHTML = `
-  <main>
-    ${nav}
-
+${nav}  
+<main>
     <div class="container-fluid my-4">
       <div class="d-flex gap-3 flex-wrap justify-content-center">
-        ${listePersonnes()}
+        ${ListeArticles()}
       </div>
     </div>
   </main>
