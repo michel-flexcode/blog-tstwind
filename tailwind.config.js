e951073f8cf0d05af0b03c8bfff77080bdff1576
michel-flexcode <michelseocode@gmail.com>
2023-11-15 14:50:14 +0100

    V1TailConfig2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
